46ad70f8de8f9ff4c281f08f388eb98c304a2fb3
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-13 17:03:22 -0300

    feat: add categories component with dynamic category cards to About page Only for dev

---
+++ VicePenguins/src/components/categories/categories_card.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps<{
+  title: string;
+  icon: any;
+  url: string;
+}>();
+</script>
+
+<template>
+  <a :href="url" class="box has-text-centered">
+    <component :is="icon" class="icon is-large" />
+    <p>{{ title }}</p>
+  </a>
+</template>
+
+<style scoped>
+.icon {
+  width: 2rem;
+  height: 2rem;
+}
+</style>
---
+++ VicePenguins/src/components/categories/categories_data.ts
@@ -0,0 +1,19 @@
+import { UserIcon, ArchiveBoxArrowDownIcon, MapIcon } from "@heroicons/vue/24/outline";
+
+export const categories = [
+  {
+    title: "Mods",
+    icon: ArchiveBoxArrowDownIcon,
+    url: "/mods",
+  },
+  {
+    title: "Skins",
+    icon: UserIcon,
+    url: "/skins",
+  },
+  {
+    title: "Maps",
+    icon: MapIcon,
+    url: "/maps",
+  }
+];
