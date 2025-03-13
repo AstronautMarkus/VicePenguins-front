84dd19b7ffa5cf9ea71c55712d71ffa3f2c80f5b
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-13 16:44:10 -0300

    feat: created vicepenguins logo + navbar items only for test

--- VicePenguins/src/components/Navbar.vue
+++ VicePenguins/src/components/Navbar.vue
@@ -1,20 +1,23 @@
 <script setup>
 import { useThemeConsumer } from '../composables/theme';
-import { SunIcon, MoonIcon } from '@heroicons/vue/24/solid';
-
+import { SunIcon, MoonIcon } from '@heroicons/vue/24/outline';
 const { theme, toggleTheme } = useThemeConsumer();
 </script>
 
 <template>
-  <nav class="navbar is-primary">
+  <nav class="navbar is-dark">
     <div class="navbar-brand">
-      <RouterLink class="navbar-item" to="/">Inicio</RouterLink>
-      <RouterLink class="navbar-item" to="/about">Acerca de</RouterLink>
+      <RouterLink class="navbar-item" to="/">
+        <img src="/public/img/logo/vicepenguins_logo.png" alt="vicepenguins" class="logo">
+      </RouterLink>
+      <RouterLink class="navbar-item" to="/">Mods</RouterLink>
+      <RouterLink class="navbar-item" to="/">Skins</RouterLink>
+      <RouterLink class="navbar-item" to="/">Texture Packs</RouterLink>     
     </div>
     <div class="navbar-end">
-      <RouterLink class="navbar-item" to="/about">Iniciar sesión</RouterLink>
-      <RouterLink class="navbar-item" to="/about">Registrarse</RouterLink>
-      <button class="button is-light icon-button" @click="toggleTheme">
+      <RouterLink class="navbar-item" to="/">Iniciar sesión</RouterLink>
+      <RouterLink class="navbar-item" to="/">Registrarse</RouterLink>
+      <button class="button is-dark icon-button" @click="toggleTheme">
         <span v-if="theme === 'light'" class="icon">
           <MoonIcon class="w-6 h-6 text-gray-900" />
         </span>
@@ -47,5 +50,10 @@ const { theme, toggleTheme } = useThemeConsumer();
   padding: 0;
   cursor: pointer;
   margin: 0 1.5rem;
+}
+
+.logo {
+  height: auto;
+  margin-right: 0.5rem;
 }
 </style>
