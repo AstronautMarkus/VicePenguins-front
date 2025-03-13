2e3bf73ce870e50800777820778d0f2dd8934219
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-13 02:09:29 -0300

    feat: update page titles in router and remove default title from index.html

--- VicePenguins/src/router.ts
+++ VicePenguins/src/router.ts
@@ -3,13 +3,18 @@ import Home from './pages/Home.vue';
 import About from './pages/About.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
+  { path: '/', component: Home, meta: { title: 'Inicio' } },
+  { path: '/about', component: About, meta: { title: 'Sobre' } },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, _from, next) => {
+  document.title = (to.meta.title as string) || 'Title';
+  next();
 });
 
 export default router;
