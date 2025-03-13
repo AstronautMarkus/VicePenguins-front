b4dfb98c0276b7c0270a2566597513cfbd63976f
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-13 16:48:53 -0300

    refactor: simplify Footer component structure and styling WIP

--- VicePenguins/src/components/Footer.vue
+++ VicePenguins/src/components/Footer.vue
@@ -1,16 +1,10 @@
-  <template>
-    <footer>
+<template>
+  <footer class="footer has-background-dark has-text-white-ter">
+    <div class="content has-text-centered">
       <p>© 2025 - Todos los derechos reservados</p>
-    </footer>
-  </template>
-  
-  <style scoped>
-  footer {
-    background: #222;
-    color: white;
-    text-align: center;
-    padding: 1rem;
-    margin-top: 2rem;
-  }
-  </style>
-  
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+</style>
