2e69dfdaa02250c9603098110ae6185b0c14c383
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-13 23:31:30 -0300

    feat: add search results section to SearchResult.vue with initial layout

--- VicePenguins/src/pages/SearchResult.vue
+++ VicePenguins/src/pages/SearchResult.vue
@@ -5,8 +5,21 @@ import DefaultLayout from '../layouts/DefaultLayout.vue';
 <template>
 <DefaultLayout>
 
+    <section class="section">
+      <h1 class="title is-2 has-text-centered">Resultados de búsqueda</h1>
+      <div class="container">
+        <div class="card is-large">
+          <h1 class="title is-3 mt-3"></h1>
+          <div class="card-content">
+            <p>aqui irian los mocs</p>
+          </div>
+        </div>
+      </div>
+    </section>
     
 </DefaultLayout>
 </template>
 
-<style></style>
+<style scoped>
+
+</style>
