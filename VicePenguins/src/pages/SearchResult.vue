d325900385591d7102fbdac274be1d584ab25abb
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-14 16:46:53 -0300

    feat: add not-found image and update SearchResult.vue layout for better user feedback

--- VicePenguins/src/pages/SearchResult.vue
+++ VicePenguins/src/pages/SearchResult.vue
@@ -1,29 +1,43 @@
 <script setup>
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 import SearchBar from '../components/SearchBar/SearchBar.vue';
+
+
 </script>
 
 <template>
-<DefaultLayout>
-
+  <DefaultLayout>
     <section class="section">
-
+      
       <SearchBar />
 
       <h1 class="title is-2 has-text-centered">Resultados de búsqueda</h1>
+      
       <div class="container">
-        <div class="card is-large">
-          <h1 class="title is-3 mt-3"></h1>
+        <div class="card mod-card">
           <div class="card-content">
-            <p>cositas here</p>
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img src="/img/exceptions/not-found.png" style="border-radius: 5%;" alt="Mod Image" />
+                </figure>
+              </div>
+              <div class="media-content">
+                <h2 class="title is-4">Mod info</h2>
+                <p class="subtitle is-6">Mod description</p>
+                <p></p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </section>
-    
-</DefaultLayout>
+  </DefaultLayout>
 </template>
 
 <style scoped>
-
+.mod-card {
+  margin-bottom: 1rem;
+  border-radius: 8px;
+}
 </style>
