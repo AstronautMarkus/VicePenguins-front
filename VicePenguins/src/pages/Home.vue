eeb4ed539b13d05067f2126c6f879c94d8a2f223
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-13 23:35:23 -0300

    feat: integrate SearchBar component into Home.vue for improved mod searching

--- VicePenguins/src/pages/Home.vue
+++ VicePenguins/src/pages/Home.vue
@@ -1,8 +1,8 @@
 <script setup>
 import DefaultLayout from '../layouts/DefaultLayout.vue';
-import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline';
 import { categories } from '../components/categories/categories_data';
 import CategoryCard  from '../components/categories/categories_card.vue';
+import SearchBar from '../components/SearchBar/SearchBar.vue';
 </script>
 
 <template>
@@ -27,20 +27,7 @@ import CategoryCard  from '../components/categories/categories_card.vue';
           </div>
         </div>
 
-        <div class="container has-text-centered mt-4">
-          <div class="field has-addons has-addons-centered">
-            <div class="control is-expanded" style="max-width: 100vh;">
-              <input class="input is-medium" type="text" placeholder="Buscar mods...">
-            </div>
-            <div class="control">
-              <button class="button is-info is-medium">
-                <span class="icon">
-                  <MagnifyingGlassIcon class="w-6 h-6" />
-                </span>
-              </button>
-            </div>
-          </div>
-        </div>
+        <SearchBar />
 
       </div>
     </section>
