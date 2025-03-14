bb27af637847c7c0b04f2cf2ce33cb6bd398b5a5
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-13 23:44:14 -0300

    feat: add SearchBar component to SearchResult.vue for enhanced user experience

--- VicePenguins/src/pages/SearchResult.vue
+++ VicePenguins/src/pages/SearchResult.vue
@@ -1,17 +1,21 @@
 <script setup>
 import DefaultLayout from '../layouts/DefaultLayout.vue';
+import SearchBar from '../components/SearchBar/SearchBar.vue';
 </script>
 
 <template>
 <DefaultLayout>
 
     <section class="section">
+
+      <SearchBar />
+
       <h1 class="title is-2 has-text-centered">Resultados de búsqueda</h1>
       <div class="container">
         <div class="card is-large">
           <h1 class="title is-3 mt-3"></h1>
           <div class="card-content">
-            <p>aqui irian los mocs</p>
+            <p>cositas here</p>
           </div>
         </div>
       </div>
