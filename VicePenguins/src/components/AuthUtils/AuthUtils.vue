e2209d68cbb780c431d3798f73fcd0e35da41d6c
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-19 01:50:36 -0300

    feat: add home button to AuthUtils component for navigation

--- VicePenguins/src/components/AuthUtils/AuthUtils.vue
+++ VicePenguins/src/components/AuthUtils/AuthUtils.vue
@@ -1,12 +1,23 @@
 <script setup>
 import { useThemeConsumer } from '../../composables/theme';
-import { SunIcon, MoonIcon } from '@heroicons/vue/24/outline';
+import { SunIcon, MoonIcon, HomeIcon } from '@heroicons/vue/24/outline';
+import { useRouter } from 'vue-router';
 
 const { theme, toggleTheme } = useThemeConsumer();
+const router = useRouter();
+
+const goHome = () => {
+  router.push('/');
+};
 </script>
 
 <template>
   <div class="auth-theme-toggle">
+    <button class="button is-dark icon-button" @click="goHome">
+      <span class="icon">
+        <HomeIcon class="w-6 h-6 text-blue-500" />
+      </span>
+    </button>
     <button class="button is-dark icon-button" @click="toggleTheme">
       <span v-if="theme === 'light'" class="icon">
         <MoonIcon class="w-6 h-6 text-gray-900" />
@@ -24,5 +35,12 @@ const { theme, toggleTheme } = useThemeConsumer();
   top: 1rem;
   right: 1rem;
   z-index: 1000;
+  display: flex;
+  gap: 1rem;
+}
+
+.auth-theme-toggle button:first-child {
+  position: fixed;
+  left: 1rem;
 }
 </style>
