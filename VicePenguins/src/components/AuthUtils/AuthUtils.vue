a5ac36e48c5ef66f6c7dd0b87c919df767d8f5ea
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-21 16:24:04 -0300

    feat: replace Heroicons with Font Awesome icons in AuthUtils component for improved UI consistency

--- VicePenguins/src/components/AuthUtils/AuthUtils.vue
+++ VicePenguins/src/components/AuthUtils/AuthUtils.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { useThemeConsumer } from '../../composables/theme';
-import { SunIcon, MoonIcon, HomeIcon } from '@heroicons/vue/24/outline';
 import { useRouter } from 'vue-router';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faSun, faMoon, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const { theme, toggleTheme } = useThemeConsumer();
 const router = useRouter();
@@ -15,15 +16,15 @@ const goHome = () => {
   <div class="auth-theme-toggle">
     <button class="button is-dark icon-button" @click="goHome">
       <span class="icon">
-        <HomeIcon class="w-6 h-6 text-blue-500" />
+        <font-awesome-icon :icon="faHouse" class="w-6 h-6 text-blue-500" />
       </span>
     </button>
     <button class="button is-dark icon-button" @click="toggleTheme">
       <span v-if="theme === 'light'" class="icon">
-        <MoonIcon class="w-6 h-6 text-gray-900" />
+        <font-awesome-icon :icon="faMoon" class="w-6 h-6 text-gray-900" />
       </span>
       <span v-else class="icon">
-        <SunIcon class="w-6 h-6 text-yellow-500" />
+        <font-awesome-icon :icon="faSun" class="w-6 h-6 text-yellow-500" />
       </span>
     </button>
   </div>
